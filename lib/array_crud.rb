518414bb848c862ed53f3a0fc7f332130943ca61
Karen Daniela <kdaniimora@gmail.com>
2019-12-15 22:19:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  my_puppies = ["Gaia", "Luna","Whiskey", "Thor"]
+  my_puppies =("Gaia", "Luna") ["estrellita"]
  
 end
 
