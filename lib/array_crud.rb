36f406ae3b1bb2ce435467ceab60e4ee604f0f88
Karen Daniela <kdaniimora@gmail.com>
2019-12-15 22:22:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,7 @@ end
 
 def add_element_to_start_of_array(array, element)
    my_puppies ("Gaia", "Luna") ["estrellita"]
-  my_puppies<< "thor"
+  my_puppies.push "thor"
 end
 
 def remove_element_from_end_of_array(array)
